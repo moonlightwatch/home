7ad25bbfc5992b8171260681516d09163781a77e
moonlightwatch <moonlightwatch@hotmail.com>
2020-08-17 18:10:24 +0800

    添加index app

--- home/home/urls.py
+++ home/home/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+
+from index import views
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name="index"),
     path('blog/', include('blog.urls')),
     path('moon-admin/', admin.site.urls),
 ]
