a6895f1156bd310203f5290ebb410d7749766a97
moonlightwatch <moonlightwatch@hotmail.com>
2020-08-18 13:35:02 +0800

    update

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
-python3 -m pip install -r requirements.txt
-cpus=$(cat /proc/cpuinfo| grep "processor"| wc -l)
-echo "cpu核心数：" $cpus
-workers=$(((2*$cpus)+1))
-echo "设定进程数：" $workers
-cd home
-python3 manage.py makemigrations
-python3 manage.py migrate
-gunicorn -b 127.0.0.1:8000 --workers=$workers home.wsgi
+
+gunicorn -b 127.0.0.1:8000 --workers=5 home.wsgi:home
--- update.sh
+++ update.sh
@@ -9,6 +9,10 @@ if [ $old_hash = $new_hash ]
 then
     echo `date` "没有更新"
 else
+    python3 -m pip install -r requirements.txt
+    cd home
+    python3 manage.py makemigrations
+    python3 manage.py migrate
     supervisorctl restart home
     echo `date` "重启服务"
 fi
