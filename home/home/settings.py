73baccc870ee559c4a85b864d5c1450ca48ba2f0
moonlightwatch <moonlightwatch@hotmail.com>
2020-08-18 13:56:17 +0800

    update

--- home/home/settings.py
+++ home/home/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&lbpfaim%ak32868-s4gdywert&$okmo04mv=n%(gzf-_bveie'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['moonlightwatch.com']
+ALLOWED_HOSTS = ['moonlightwatch.com', '127.0.0.1']
 
 
 # Application definition
