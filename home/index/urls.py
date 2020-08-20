aa40a394338b39abedfaead80f6c4702e821609f
moonlightwatch <moonlightwatch@hotmail.com>
2020-08-20 09:10:56 +0800

    fix urls

--- home/index/urls.py
+++ home/index/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import IndexView, AboutView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
     path('about/', AboutView.as_view(), name='index')
 ]
