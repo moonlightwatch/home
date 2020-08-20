ca8a9fb13c67d30c1d0749e01f00b7abef7003e5
moonlightwatch <moonlightwatch@hotmail.com>
2020-08-20 09:12:57 +0800

    update urls

--- home/index/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import IndexView, AboutView
-
-urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('about/', AboutView.as_view(), name='index')
-]
