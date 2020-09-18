04644fdeb1d132238097e984bc4f5584a846c642
moonlightwatch <moonlightwatch@hotmail.com>
2020-09-18 17:49:06 +0800

    update

--- home/blog/views.py
+++ home/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from django.views.generic import ListView, TemplateView, DetailView
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Article, Tag, Category,Link
+from .models import Article, Tag, Category, Link
 
 # Create your views here.
 
@@ -10,11 +10,13 @@ from .models import Article, Tag, Category,Link
 def index(request):
     return HttpResponseRedirect(redirect_to="/blog/articles/")
 
+
 class ArticleList(ListView):
     template_name = "blog/article_list.html"
     model = Article
     paginate_by = 10
-    def get_queryset(self): # 重写get_queryset方法
+
+    def get_queryset(self):  # 重写get_queryset方法
         # 获取所有is_deleted为False的用户，并且以时间倒序返回数据
         return Article.objects.filter(is_draft=False).order_by('-create_time')
 
@@ -28,9 +30,11 @@ class TagList(ListView):
     template_name = "blog/tag_list.html"
     model = Tag
 
+
 class TagDetail(DetailView):
     template_name = "blog/tag.html"
     model = Tag
+
 
 class CategoryList(ListView):
     template_name = "blog/category_list.html"
@@ -40,6 +44,7 @@ class CategoryList(ListView):
 class CategoryDetail(DetailView):
     template_name = "blog/category.html"
     model = Category
+
 
 class LinkList(ListView):
     template_name = "blog/link_list.html"
