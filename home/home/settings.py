ab751b80e0d07eabec22118d91e839797b04f561
moonlightwatch <moonlightwatch@hotmail.com>
2020-09-21 15:38:19 +0800

    add photos

--- home/home/settings.py
+++ home/home/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'index.apps.IndexConfig',
     'blog.apps.BlogConfig',
+    'photos.apps.PhotosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
