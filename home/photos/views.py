1a5a3c4ca242860c40d6b72149e3eba3fd6df50a
moonlightwatch <moonlightwatch@hotmail.com>
2020-09-21 16:54:14 +0800

    update

--- home/photos/views.py
+++ home/photos/views.py
@@ -1,3 +1,12 @@
+import json
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    data={
+        "status":"ok"
+    }
+    return HttpResponse(content=json.dumps(data), content_type="application/json")
+
