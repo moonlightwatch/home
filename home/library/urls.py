c9a84ff481eb4c33282e7139413e88610a95ccd6
moonlightwatch <moonlightwatch@hotmail.com>
2020-10-12 14:00:55 +0800

    修复博客分类点击错误

--- home/library/urls.py
+++ home/library/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.BookList.as_view(), name='library_index'),
-    path('book/', views.BookDetail.as_view(), name="book"),
-    path('chapter/<int:pk>', views.ChapterDetail.as_view(), name="chapter"),
-    path('categories/', views.CategoryList.as_view(), name='categories'),
-    path('category/<int:pk>', views.CategoryDetail.as_view(), name='category'),
+    path('book/', views.BookDetail.as_view(), name="library_book"),
+    path('chapter/<int:pk>', views.ChapterDetail.as_view(), name="library_chapter"),
+    path('categories/', views.CategoryList.as_view(), name='library_categories'),
+    path('category/<int:pk>', views.CategoryDetail.as_view(), name='library_category'),
 ]
